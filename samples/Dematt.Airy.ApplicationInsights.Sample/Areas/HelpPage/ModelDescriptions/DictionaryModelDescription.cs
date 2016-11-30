d363da18ea92d370f1b6db384f49b278ecd9d4f1
Matthew Rudolph <Matthew.Rudolph@users.noreply.github.com>
2016-11-30 11:32:19 +0000

    Add sample and src folders and base projects.

---
+++ samples/Dematt.Airy.ApplicationInsights.Sample/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Dematt.Airy.ApplicationInsights.Sample.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ samples/Dematt.Airy.ApplicationInsights.Sample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Dematt.Airy.ApplicationInsights.Sample.WebApiApplication" Language="C#" %>
