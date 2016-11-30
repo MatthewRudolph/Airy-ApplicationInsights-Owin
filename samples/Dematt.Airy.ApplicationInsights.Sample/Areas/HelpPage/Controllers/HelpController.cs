1cb4ade8f3474725b310e1e79d6ecdc55769afbe
Matthew Rudolph <Matthew.Rudolph@users.noreply.github.com>
2016-11-30 22:11:02 +0000

    Change sample project to use Owin, replacing Global.cs with Startup.cs

--- samples/Dematt.Airy.ApplicationInsights.Sample/Areas/HelpPage/Controllers/HelpController.cs
+++ samples/Dematt.Airy.ApplicationInsights.Sample/Areas/HelpPage/Controllers/HelpController.cs
@@ -14,7 +14,7 @@ namespace Dematt.Airy.ApplicationInsights.Sample.Areas.HelpPage.Controllers
         private const string ErrorViewName = "Error";
 
         public HelpController()
-            : this(GlobalConfiguration.Configuration)
+            : this(Startup.HttpConfig)
         {
         }
 
---
+++ samples/Dematt.Airy.ApplicationInsights.Sample/Startup.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using Owin;
+
+namespace Dematt.Airy.ApplicationInsights.Sample
+{
+    public class Startup
+    {
+        // Having this as static allows use to register the MVC areas correctly avoiding the need to use GlobalConfiguration.Configuration.
+        // And we really don't want to use GlobalConfiguration.Configuration when using Owin.
+        // http://stackoverflow.com/questions/18921215/cant-get-asp-net-web-api-2-help-pages-working-when-using-owin
+        public static HttpConfiguration HttpConfig { get; private set; }
+
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfig = new HttpConfiguration();
+
+            // Configure owin to use WebApi.
+            app.UseWebApi(HttpConfig);
+
+            // Configure WebApi as required for the default Visual Studio Template.
+            WebApiConfig.Register(HttpConfig);
+
+            // Configure all the required MVC stuff for the default Visual Studio Template.
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
