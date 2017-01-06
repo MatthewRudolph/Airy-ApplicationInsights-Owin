4fb399eb91fc918ab95d6301f1407e3be4a1a7b6
Matthew Rudolph <Matthew.Rudolph@users.noreply.github.com>
2017-01-06 19:16:32 +0000

    Add detail comments to the Startup configuration and the matching AppBuilder extension method.

--- src/Dematt.Airy.ApplicationInsights.Owin/AppBuilderExtension.cs
+++ src/Dematt.Airy.ApplicationInsights.Owin/AppBuilderExtension.cs
@@ -23,11 +23,20 @@ namespace Dematt.Airy.ApplicationInsights.Owin
             telemetryClient = telemetryClient ?? new TelemetryClient();
 
             // ***IT IS IMPORTANT TO ADD ANY CUSTOM EXCEPTION LOGGERS BEFORE ANY OTHER CONFIGURATION***
+            // Web API application insights error logging.
             httpConfiguration.Services.Add(typeof(IExceptionLogger), new WebApiExceptionLogger(telemetryClient));
 
+            // Web API application insights controller and action name capture.
             httpConfiguration.Filters.Add(new WebApiRouteFilterAttribute(options));
+
+            // ASP.Net MVC application insights controller and action name capture.
             GlobalFilters.Filters.Add(new MvcRouteFilterAttribute(options));
+
+            // ASP.Net MVC application insights error logging.
             GlobalFilters.Filters.Add(new MvcExceptionHandler(telemetryClient));
+
+            // Add the request tracking middleware that actual sends the TelemtryRequests for all owin requests both Web API and ASP.Net MVC.
+            // This also tracks any errors that occur in the owin pipeline, however it will not capture errors that occur in any middleware that is added to the owin pipeline before it.
             builder.Use<RequestTrackingMiddleware>(telemetryClient);
 
             return builder;
